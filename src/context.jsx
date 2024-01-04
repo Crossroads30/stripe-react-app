043f69a262290940a0a61e7f01f2ac4dedfb38dc
crossroads30 <crossroads30@mail.ru>
2024-01-04 17:49:21 +0300

    feat: set global context

--- src/context.jsx
+++ src/context.jsx
@@ -1,2 +1,14 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from 'react'
 import sublinks from './data'
+
+const AppContext = createContext()
+
+//custom hook
+export const useGlobalContext = () => useContext(AppContext)
+
+const AppProvider = ({children}) => {
+const [links, setLinks] = useState(sublinks)
+
+  return <AppContext.Provider value={'hello'}>{children}</AppContext.Provider>
+}
+export default AppProvider

563f442caa8d1a3a7ba8c7d0539fa3047d8997c2
crossroads30 <crossroads30@mail.ru>
2024-01-04 23:19:20 +0300

    fix component structure

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,24 +6,25 @@ const Hero = () => {
 	const { closeSubmenu } = useGlobalContext()
 
 	return (
-		<main className='hero'>
+		<section className='hero' onMouseOver={closeSubmenu}>
 			<div className='hero-center'>
 				<article className='hero-info'>
-					<h1>Payments infrastructure for the internet</h1>
+					<h1>
+						Payments infrastructure <br />
+						for the internet
+					</h1>
 					<p>
-						Millions of companies of all sizes-form startups to Fortune 500s-use
-						Stripe`s software and APl to accept payments, send payouts, and
-						manage their bussiness online.
+						Millions of companies of all sizes—from startups to Fortune 500s—use
+						Stripe’s software and APIs to accept payments, send payouts, and
+						manage their businesses online.
 					</p>
-					<button className='btn' type='button'>
-						start now
-					</button>
+					<button className='btn'>Start now</button>
 				</article>
 				<article className='hero-images'>
-					<img src={phoneImg} alt='phone' />
+					<img src={phoneImg} className='phone-img' alt='phone' />
 				</article>
 			</div>
-		</main>
+		</section>
 	)
 }
 
